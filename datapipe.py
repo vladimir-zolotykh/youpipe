6b633885aeae982c74ce1e830d8fa1223a9f9240
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-15 10:11:35 +0300

    Add iter_handle, iter_line, iter_select, main guard

--- datapipe.py
+++ datapipe.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from typing import Iterator
+from typing import Iterator, FileIO
 import os
+import re
+import gzip
+import bz2
 
 
 def iter_logfile(top) -> Iterator[str]:
     """Iterate logfiles
     www/bar/access-log-0208.bz2
     www/bar/access-log
+    www/foo/access-log-0208.gz
     """
     for dir, _, names in os.walk(top):
         for name in names:
             yield os.path.join(dir, name)
 
 
+def iter_handle(logfiles: Iterator[str]) -> Iterator[FileIO]:
+    for log in logfiles:
+        fd: FileIO
+        if log.endswith(".gz"):
+            fd = gzip.open(log)
+        elif log.endswith(".bz2"):
+            fd = bz2.open(log)
+        else:
+            fd = open(log)
+        yield fd
+
+
+def iter_line(log_hadle: Iterator[FileIO]) -> Iterator[str]:
+    for h in log_hadle:
+        yield from h
+
+
+def iter_select(line: Iterator[str], pat: str) -> Iterator[str]:
+    if re.search(pat, line):
+        yield line
+
+
 if __name__ == "__main__":
-    for path in iter_logfile("www"):
-        print(path)
+    logfiles = iter_logfile("www")
+    handles = iter_handle(logfiles)
+    lines = iter_line(handles)
+    selected_lines = iter_select(lines, "Python")
+    for line in selected_lines:
+        print(line)
