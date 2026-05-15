0cdb2da15e98f862f1d6cc4b02bf950e266e67b1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-15 10:36:38 +0300

    Refactor iter_handle, iter_select
    -            fd = gzip.open(log)
    +            fd = gzip.open(log, "rt")

--- datapipe.py
+++ datapipe.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from typing import Iterator, FileIO
+from typing import Union, Iterator, TextIO, BinaryIO
 import os
 import re
 import gzip
 import bz2
 
+FileIO = Union[BinaryIO, TextIO]
 
-def iter_logfile(top) -> Iterator[str]:
+
+def iter_logfile(top, pat: str) -> Iterator[str]:
     """Iterate logfiles
     www/bar/access-log-0208.bz2
     www/bar/access-log
@@ -16,18 +18,19 @@ def iter_logfile(top) -> Iterator[str]:
     """
     for dir, _, names in os.walk(top):
         for name in names:
-            yield os.path.join(dir, name)
+            if re.match(pat, name):
+                yield os.path.join(dir, name)
 
 
 def iter_handle(logfiles: Iterator[str]) -> Iterator[FileIO]:
     for log in logfiles:
         fd: FileIO
         if log.endswith(".gz"):
-            fd = gzip.open(log)
+            fd = gzip.open(log, "rt")
         elif log.endswith(".bz2"):
-            fd = bz2.open(log)
+            fd = bz2.open(log, "rt")
         else:
-            fd = open(log)
+            fd = open(log, "rt")
         yield fd
 
 
@@ -36,15 +39,16 @@ def iter_line(log_hadle: Iterator[FileIO]) -> Iterator[str]:
         yield from h
 
 
-def iter_select(line: Iterator[str], pat: str) -> Iterator[str]:
-    if re.search(pat, line):
-        yield line
+def iter_select(lines: Iterator[str], pat: str) -> Iterator[str]:
+    for line in lines:
+        if re.search(pat, line):
+            yield line
 
 
 if __name__ == "__main__":
-    logfiles = iter_logfile("www")
+    logfiles = iter_logfile("www", "access-log")
     handles = iter_handle(logfiles)
     lines = iter_line(handles)
-    selected_lines = iter_select(lines, "Python")
+    selected_lines = iter_select(lines, "bookplug.gif")
     for line in selected_lines:
-        print(line)
+        print(line, end="")
